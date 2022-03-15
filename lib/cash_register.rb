bd1b31dc581a2fa276a4d1ab9417121abf090988
Stefan Inman <96351972+Anon-67@users.noreply.github.com>
2022-03-15 13:58:40 -0500

    commit tree e5f8c0be9aa4d35b7b5303093933325afe757208

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
 
+
+class CashRegister
+    attr_accessor :total, :discount, :items, :new_item
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.new_item =(price * quantity)
+        self.total += (price * quantity)
+        quantity.times do
+            self.items << title
+        end
+
+    end
+
+    def apply_discount
+        if discount != 0
+            discount_percent = ((100 - @discount).to_f/100)
+            self.total= (self.total * discount_percent).to_int
+            "After the discount, the total comes to $#{total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        self.total -= new_item
+    end
+
+
+end
